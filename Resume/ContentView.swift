0cdebef26351c4069a4239dd7d9956f0d18dea5b
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-03-14 19:29:31 +1030

    resume

--- Resume/ContentView.swift
+++ Resume/ContentView.swift
@@ -9,53 +9,165 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        VStack(alignment: .leading, spacing: 0) {
+            
+            
+            HStack{
+                Image("LariProfile")
+                    .resizable()
+                    .frame(width: 90, height: 90)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                  
+                
+                VStack{
+                    Text("Larissa Rocha")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(.brown)
+                        .padding(.top)
+                    Text("Tech enthusiast")
+                        .foregroundStyle(.secondary)
                 }
-                .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+            Spacer()
+            Group {
+                Text("Skills")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
+                    .foregroundColor(.orange)
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .frame(height: 1)
+                    .padding(.bottom)
+                HStack {
+                    Text("App Developer")
+                        .bold()
+                        .font(.title3)
+                    Spacer()
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                Text("Swift & Flutter")
+                    .foregroundStyle(.secondary)
+                    .padding(.bottom)
+                
+            }
+            
+            
+            Group {
+                Text("Work Experience")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
+                    .foregroundColor(.orange)
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .frame(height: 1)
+                    .padding(.bottom)
+                HStack {
+                    Text("LIBERTY Steel Group")
+                        .bold()
+                        .font(.title3)
+                    Spacer()
+                    Text("2023 - Present")
                 }
+                Text("Safety Specialist")
+                    .foregroundStyle(.secondary)
+                    .padding(.bottom)
+                
+                HStack{
+                    Text("FPF Tech")
+                        .bold()
+                        .font(.title3)
+                    Spacer()
+                    Text("2021 - 2022")
+                }
+                Text("Software Developer")
+                    .foregroundStyle(.secondary)
+                    .padding(.bottom)
+                
             }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+            
+            
+            Spacer()
+            Group {
+                
+                Text("Education")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.orange)
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .frame(height: 1)
+                    .padding(.bottom)
+                    
+                HStack{
+                    Text("Bachelor's Degree")
+                        .bold()
+                        .font(.title3)
+                    Spacer()
+                    Text("2009 - 2014")
+                }
+                Text("Uninorte - Laureate International")
+                Text("Environmental Engineering")
+                    .foregroundStyle(.secondary)
+                    .padding(.bottom)
+                HStack{
+                    Text("Posgraduate Degree")
+                        .bold()
+                        .font(.title3)
+                    Spacer()
+                    Text("2015 - 2016")
+                }
+                Text("Uninorte - Laureate International")
+                
+                Text("Safety Engineering")
+                    .foregroundStyle(.secondary)
+                    .padding(.bottom)
+                
             }
+            Spacer()
+            Group {
+                Text("Interest")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
+                    .foregroundColor(.orange)
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .frame(height: 1)
+                    .padding(.bottom)
+            }
+            
+            HStack(content: {
+                Spacer()
+                VStack(content: {
+                    Text("📚")
+                        .font(.system(size: 70))
+                    Text("Reading books")
+                        .bold()
+                })
+                Spacer()
+                VStack(content: {
+                    Text("👩🏻‍💻")
+                        .font(.system(size: 70))
+                    Text("Coding")
+                        .bold()
+                })
+                Spacer()
+                VStack(content: {
+                    Text("🛫")
+                        .font(.system(size: 70))
+                    Text("Traveling")
+                        .bold()
+                })
+                Spacer()
+            })
+            
         }
+        .padding()
     }
+    
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+    
 }
